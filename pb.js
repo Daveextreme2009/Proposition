11c5e8fe5ed95feb17ceabac535ee05dd049bc44
Daveextreme2009 <155586106+Daveextreme2009@users.noreply.github.com>
2024-01-07 21:28:35 +0100

    Add files via upload

---
+++ pb.js
@@ -0,0 +1,6 @@
+const file = document.getElementById('btn');
+
+
+  file.addEventListener("click", function() {
+    document.location.href = "C:/Users/strs/Desktop/Work/Premium Bank/Login.html";
+  });
